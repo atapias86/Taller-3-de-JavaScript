8ce27a2e14c138c0c06aab3669e4c07bcb0c0d84
atapias86 <atapias86@misena.edu.co>
2022-09-29 18:30:57 -0500

    index, style y main del taller 3 de python a java script finalizado

--- js/main.js
+++ js/main.js
@@ -1,21 +1,37 @@
-function parametro_num(parametros) {
+function parametro_num(parametro) {
     document.querySelector("#form_01").reset();
     favDialog.showModal();
     favDialog.dataset.ciclo = parametro;
 }
-function parametro_num(parametros) {
+function parametros(parametros_votos){
     document.querySelector("#form_09").reset();
+    console.log(parametros_votos);
     favDialog_09.showModal();
-    favDialog_09.dataset.votos = parametros;
+    favDialog_09.dataset.votos = parametros_votos;
+}
+function parametro_trans(parametros_plac) {
+    document.querySelector("#dataTable_calco").innerHTML = "";
+    document.querySelector("#form_11").reset();
+    favDialog_11.showModal();
+    favDialog_11.dataset.placas = parametros_plac;
+}
+function parametros2(parametros_votos2){
+    document.querySelector("#form_12").reset();
+    favDialog_12.showModal();
+    favDialog_12.dataset.votos2 = parametros_votos2;
 }
 addEventListener("DOMContentLoaded", (e) => {
     const favDialog = document.getElementById('favDialog');
     const favDialog_09 = document.getElementById('favDialog_09');
+    const favDialog_11 = document.getElementById('favDialog_11');
     const favDialog_ejer02 = document.getElementById('favDialog_ejer02');
     const favDialog_ejer03 = document.getElementById('favDialog_ejer03');
     const favDialog_ejer05 = document.getElementById('favDialog_ejer05');
     const favDialog_ejer07 = document.getElementById('favDialog_ejer07');
     const favDialog_ejer08 = document.getElementById('favDialog_ejer08');
+    const favDialog_ejer10 = document.getElementById('favDialog_ejer10');
+    const favDialog_12 = document.getElementById('favDialog_12');
+
     let calcular_ejer1 = document.querySelector("#form_01");
     let formdialogo_ejer_1 = document.querySelector("#dialog");
     let calcular_ejer02 = document.querySelector("#btn_incio_ejer02");
@@ -33,6 +49,12 @@ addEventListener("DOMContentLoaded", (e) => {
     let formdialogo_ejer_08 = document.querySelector("#dialogo_08");
     let calcular_ejer9 = document.querySelector("#form_09");
     let formdialogo_ejer_9 = document.querySelector("#dialog_09");
+    let calcular_ejer10 = document.querySelector("#btn_incio_ejer10");
+    let formdialogo_ejer_10 = document.querySelector("#dialogo_10");
+    let calcular_ejer11 = document.querySelector("#form_11");
+    let formdialogo_ejer_11 = document.querySelector("#dialogo_11");
+    let calcular_ejer12 = document.querySelector("#form_12");
+    let formdialogo_ejer_12 = document.querySelector("#dialog_12");
     let count = 0;
     let cont = 1;
     let coun = 1;
@@ -40,6 +62,8 @@ addEventListener("DOMContentLoaded", (e) => {
     let dama = 1;
     let caballeros = 7;
     let A = 0, B = 0, C = 0;
+    let num_votos = 0;
+    let por_vot = (num_votos * 0.51);
     calcular_ejer1.addEventListener("submit", (e) => {
         e.preventDefault();
         let parametro = parseInt(document.querySelector("#parametro").value);
@@ -293,8 +317,8 @@ addEventListener("DOMContentLoaded", (e) => {
 
     calcular_ejer9.addEventListener("submit", (e) => {
         e.preventDefault();
-        let parametros = parseInt(document.querySelector("#parametro_09").value);
-        parametro_num(parametros);
+        let parametros_votos = parseInt(document.querySelector("#parametros_09").value);
+        parametros(parametros_votos);
     });
     formdialogo_ejer_9.addEventListener("submit", (e) => {
         e.preventDefault();
@@ -302,15 +326,18 @@ addEventListener("DOMContentLoaded", (e) => {
         let num = Number(favDialog_09.dataset.votos);
         let data = Object.fromEntries(new FormData(e.target));
         let num1 = Number(data.num_ejer9);
-        if(num1 == 1){
-
-        }else if(num1 == 2){
-
-        }else if(num1 == 3)
+        if (num1 == 1) {
+            A++;
+        } else if (num1 == 2) {
+            B++;
+        } else if (num1 == 3) {
+            C++;
+        }
+        num_votos++;
         num--;
         if (num) {
             formdialogo_ejer_9.reset();
-            favDialog_09.dataset.ciclo = num;
+            favDialog_09.dataset.votos = num;
             favDialog_09.close();
             setTimeout(() => {
                 favDialog_09.showModal();
@@ -319,6 +346,220 @@ addEventListener("DOMContentLoaded", (e) => {
         } else {
             formdialogo_ejer_9.reset();
             favDialog_09.close();
+            let pa = A * 0.51;
+            let pb = B * 0.51;
+            let pc = C * 0.51;
+            if (A > B && A > C) {
+                if (pa >= por_vot) {
+                    resultado.value = "EL CANDIDATO A GANA LAS ELECCIONES CON " + A + " BOTOS";
+                }
+            } else if (B > A && B > C) {
+                if (pb >= por_vot) {
+                    resultado.value = "EL CANDIDATO B GANA LAS ELECCIONES CON " + B + " BOTOS";
+                }
+            } else if (C > A && C > B) {
+                if (pc >= por_vot) {
+                    resultado.value = "EL CANDIDATO C GANA LAS ELECCIONES CON " + C + " BOTOS";
+                }
+            } else {
+                resultado.value = "SE DEVE REALISAR UNA SEGUNDA BUELTA ", "\n";
+            }
+            A = 0, B = 0, C = 0;
+        }
+    });
+    calcular_ejer10.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector("#dataTable_transito").innerHTML = "";
+        favDialog_ejer10.showModal();
+    });
+    let turismo = 0;
+    let autobus = 0;
+    let camion = 0;
+    let motocicleta = 0;
+    formdialogo_ejer_10.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let num = Number(formdialogo_ejer_10.dataset.transito);
+        let opc = document.querySelector("#vehiculo").value;
+        if (opc == "Turismo") {
+            turismo++;
+        } else if (opc == "Autobús") {
+            autobus++;
+        } else if (opc == "Camión") {
+            camion++;
+        } else if (opc == "Motocicleta") {
+            motocicleta++;
+        }
+
+        if (num > cont) {
+            formdialogo_ejer_10.reset();
+            cont++;
+        } else {
+            favDialog_ejer10.close();
+            formdialogo_ejer_10.reset();
+            if (turismo >= 0) {
+                let plantillas = `
+            <tr>
+                <td>${1}</td>
+                <td>${'Turismo'}</td>
+                <td>${turismo}</td>
+            </tr>`;
+                document.querySelector("#dataTable_transito").insertAdjacentHTML("beforeend", plantillas);
+            }
+            if (autobus >= 0) {
+                let plantillas = `
+            <tr>
+                <td>${2}</td>
+                <td>${'Autobús'}</td>
+                <td>${autobus}</td>
+            </tr>`;
+                document.querySelector("#dataTable_transito").insertAdjacentHTML("beforeend", plantillas);
+            }
+            if (camion >= 0) {
+                let plantillas = `
+            <tr>
+                <td>${3}</td>
+                <td>${'Camión'}</td>
+                <td>${camion}</td>
+            </tr>`;
+                document.querySelector("#dataTable_transito").insertAdjacentHTML("beforeend", plantillas);
+            }
+            if (motocicleta >= 0) {
+                let plantillas = `
+            <tr>
+                <td>${4}</td>
+                <td>${'Motocicleta'}</td>
+                <td>${motocicleta}</td>
+            </tr>`;
+                document.querySelector("#dataTable_transito").insertAdjacentHTML("beforeend", plantillas);
+            }
+            cont = 1;
+        }
+    });
+    let placas_1_2 = 0;
+    let placas_3_4 = 0;
+    let placas_5_6 = 0;
+    let placas_7_8 = 0;
+    let placas_9_0 = 0;
+    calcular_ejer11.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let parametros_plac = parseInt(document.querySelector("#parametro_placas").value);
+        parametro_trans(parametros_plac);
+    });
+    formdialogo_ejer_11.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let num = Number(favDialog_11.dataset.placas);
+        let data = Object.fromEntries(new FormData(e.target));
+        let placa = Number(data.num_placa);
+        if(placa == 1 || placa == 2){
+            placas_1_2++;
+        }else if(placa == 3 || placa == 4){
+            placas_3_4++;
+        }else if(placa == 5 || placa == 6){
+            placas_5_6++;
+        }else if(placa == 7 || placa == 8){
+            placas_7_8++;
+        }else if(placa == 9 || placa == 0){
+            placas_9_0++;
+        }
+        num--;
+        if (num) {
+            formdialogo_ejer_11.reset();
+            favDialog_11.dataset.placas = num;
+            favDialog_11.close();
+            setTimeout(() => {
+                favDialog_11.showModal();
+            }, 500);
+
+        } else {
+            formdialogo_ejer_11.reset();
+            favDialog_11.close();
+            let plantillas = `
+            <tr>
+                <td>${1}</td>
+                <td>${'Amarillo'}</td>
+                <td>${'1 o 2'}</td>
+                <td>${placas_1_2}</td>
+            </tr>
+            <tr>
+                <td>${2}</td>
+                <td>${'Rosa'}</td>
+                <td>${'3 o 4'}</td>
+                <td>${placas_3_4}</td>
+            </tr>
+            <tr>
+                <td>${3}</td>
+                <td>${'Roja'}</td>
+                <td>${'5 o 6'}</td>
+                <td>${placas_5_6}</td>
+            </tr>
+            <tr>
+                <td>${4}</td>
+                <td>${'Verde'}</td>
+                <td>${'7 o 8'}</td>
+                <td>${placas_7_8}</td>
+            </tr>
+            <tr>
+                <td>${5}</td>
+                <td>${'Azul'}</td>
+                <td>${'9 o 0'}</td>
+                <td>${placas_9_0}</td>
+            </tr>
+            `
+            ;
+            document.querySelector("#dataTable_calco").insertAdjacentHTML("beforeend", plantillas);
+        }
+    });
+
+    calcular_ejer12.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let parametros_votos2 = parseInt(document.querySelector("#parametros_12").value);
+        parametros2(parametros_votos2);
+    });
+    formdialogo_ejer_12.addEventListener("submit", (e) => {
+        e.preventDefault();
+        var resultado = document.querySelector("#text_resultado_ejer12");
+        let num = Number(favDialog_12.dataset.votos2);
+        let data = Object.fromEntries(new FormData(e.target));
+        let num1 = Number(data.num_ejer12);
+        if (num1 == 1) {
+            A++;
+        } else if (num1 == 2) {
+            B++;
+        } else if (num1 == 3) {
+            C++;
+        }
+        num_votos++;
+        num--;
+        if (num) {
+            formdialogo_ejer_12.reset();
+            favDialog_12.dataset.votos2 = num;
+            favDialog_12.close();
+            setTimeout(() => {
+                favDialog_12.showModal();
+            }, 500);
+
+        } else {
+            formdialogo_ejer_12.reset();
+            favDialog_12.close();
+            let pa = A * 0.51;
+            let pb = B * 0.51;
+            let pc = C * 0.51;
+            if (A > B && A > C) {
+                if (pa >= por_vot) {
+                    resultado.value = "EL CANDIDATO A GANA LAS ELECCIONES CON " + A + " BOTOS";
+                }
+            } else if (B > A && B > C) {
+                if (pb >= por_vot) {
+                    resultado.value = "EL CANDIDATO B GANA LAS ELECCIONES CON " + B + " BOTOS";
+                }
+            } else if (C > A && C > B) {
+                if (pc >= por_vot) {
+                    resultado.value = "EL CANDIDATO C GANA LAS ELECCIONES CON " + C + " BOTOS";
+                }
+            } else {
+                resultado.value = "SE DEVE REALISAR UNA SEGUNDA BUELTA ", "\n";
+            }
+            A = 0, B = 0, C = 0;
         }
     });
 });
